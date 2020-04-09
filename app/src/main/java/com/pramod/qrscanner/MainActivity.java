d3b1d4ef2110dfa2ca362b0920acc4fba401bc91
Pramod Kumar Yadav <44602671+itzpkyadav@users.noreply.github.com>
2020-04-09 20:05:33 +0530

    Update MainActivity.java

--- app/src/main/java/com/pramod/qrscanner/MainActivity.java
+++ app/src/main/java/com/pramod/qrscanner/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private Button btnScanBarcode;
     private static final int REQUEST_CODE_SCAN = 101;
+    private static final int PERMISSION_REQUEST_CODE = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,13 +32,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnScanBarcode.setOnClickListener(this);
     }
 
-    @Override
+     @Override
     public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btnScanBarcode:
+        if (v.getId() == R.id.btnScanBarcode) {
+            if (checkPermission()) {
+                //main logic or main code
+                // . write your main code to execute, It will execute if the permission is already given.
                 Intent intent = new Intent(MainActivity.this, QrCodeActivity.class);
                 startActivityForResult(intent, REQUEST_CODE_SCAN);
-                break;
+            } else {
+                requestPermission();
+            }
         }
     }
 
@@ -85,5 +90,52 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             });
             alertDialog.show();
         }
+    }
+    
+      private boolean checkPermission() {
+        // Permission is not granted
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.CAMERA},
+                PERMISSION_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(getApplicationContext(), "Permission Granted", Toast.LENGTH_SHORT).show();
+
+                // main logic
+            } else {
+                Toast.makeText(getApplicationContext(), "Permission Denied", Toast.LENGTH_SHORT).show();
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                            != PackageManager.PERMISSION_GRANTED) {
+                        showMessageOKCancel("You need to allow access permissions",
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                            requestPermission();
+                                        }
+                                    }
+                                });
+                    }
+                }
+            }
+        }
+    }
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(MainActivity.this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
     }
 }
